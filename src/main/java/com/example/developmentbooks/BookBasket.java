8308c6cfb157b16a02e03977aa8048b16e692688
YacineALI <yacine.ali@sii.be>
2022-11-03 07:00:18 +0100

    added a map to know the number of times that each book among the 5

--- src/main/java/com/example/developmentbooks/BookBasket.java
+++ src/main/java/com/example/developmentbooks/BookBasket.java
@@ -1,6 +1,7 @@
 package com.example.developmentbooks;
 
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -8,8 +9,27 @@ public class BookBasket {
 
     private static final int BOOK_PRICE = 50;
 
+    private Map<Book, Integer> booksCountMap;
+
     private Map<Integer, Double> discountRates =  Map.of(1, 0.0, 2, 0.05, 3, 0.1, 4, 0.2, 5, 0.25);
 
+    private Map<Book, Integer> getBooksCountMap(List<Book> books){
+        booksCountMap = new HashMap<>();
+        for (Book book: books)
+        {
+            if (booksCountMap.containsKey(book))
+            {
+                booksCountMap.put(book, booksCountMap.get(book) + 1);
+            }
+            else
+            {
+                booksCountMap.put(book , 1);
+            }
+        }
+        return booksCountMap;
+
+
+    }
 
 
     public double getBookTotalPrice(List<Book> books){
