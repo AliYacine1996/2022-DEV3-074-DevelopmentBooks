0f22fafe4ee9d6c37bd5f43597c9ff18303cade4
YacineALI <yacine.ali@sii.be>
2022-11-03 06:55:49 +0100

    start of the implementation method getBookTotalPrice

--- src/main/java/com/example/developmentbooks/BookBasket.java
+++ src/main/java/com/example/developmentbooks/BookBasket.java
@@ -2,12 +2,19 @@ package com.example.developmentbooks;
 
 
 import java.util.List;
+import java.util.Map;
 
 public class BookBasket {
 
+    private static final int BOOK_PRICE = 50;
+
+    private Map<Integer, Double> discountRates =  Map.of(1, 0.0, 2, 0.05, 3, 0.1, 4, 0.2, 5, 0.25);
+
+
 
     public double getBookTotalPrice(List<Book> books){
-        return 0;
+        int quantity = books.size();
+        return BOOK_PRICE*quantity - this.discountRates.get(quantity)*BOOK_PRICE;
     }
 
 }
