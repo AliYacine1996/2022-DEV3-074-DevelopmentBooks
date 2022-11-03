b368f9816811ce430982a422b7dc6a85a4654cfe
YacineALI <yacine.ali@sii.be>
2022-11-03 07:39:14 +0100

    refactor and indent code

--- src/main/java/com/example/developmentbooks/BookBasket.java
+++ src/main/java/com/example/developmentbooks/BookBasket.java
@@ -9,58 +9,50 @@ public class BookBasket {
 
     private Map<Book, Integer> booksCountMap;
 
-    private Map<Integer, Double> discountRates ;
+    private Map<Integer, Double> discountRates;
 
     private List<Integer> discountGroups;
 
     public BookBasket() {
-        this.discountRates =  Map.of(1, 0.0, 2, 0.05, 3, 0.1, 4, 0.2, 5, 0.25);
+        this.discountRates = Map.of(1, 0.0, 2, 0.05, 3, 0.1, 4, 0.2, 5, 0.25);
     }
 
-    private Map<Book, Integer> getBooksCountMap(List<Book> books){
+    private Map<Book, Integer> getBooksCountMap(List<Book> books) {
         booksCountMap = new HashMap<>();
-        for (Book book: books)
-        {
-            if (booksCountMap.containsKey(book))
-            {
+        for (Book book : books) {
+            if (booksCountMap.containsKey(book)) {
                 booksCountMap.put(book, booksCountMap.get(book) + 1);
-            }
-            else
-            {
-                booksCountMap.put(book , 1);
+            } else {
+                booksCountMap.put(book, 1);
             }
         }
         return booksCountMap;
 
 
     }
-    private List<Integer> getDiscountGroups(Map<Book, Integer> booksCountMap)
-    {
+
+    private List<Integer> getDiscountGroups(Map<Book, Integer> booksCountMap) {
         discountGroups = new ArrayList<>();
 
-        while (booksCountMap.size()>0)
-        {
+        while (booksCountMap.size() > 0) {
             discountGroups.add(booksCountMap.size());
             Set<Book> toBeRemoved = new HashSet<>();
-            for (Book key : booksCountMap.keySet())
-            {
-                if (booksCountMap.get(key)==1)
+            for (Book key : booksCountMap.keySet()) {
+                if (booksCountMap.get(key) == 1)
                     toBeRemoved.add(key);
                 else
-                    booksCountMap.put(key, booksCountMap.get(key)-1);
+                    booksCountMap.put(key, booksCountMap.get(key) - 1);
             }
             booksCountMap.keySet().removeAll(toBeRemoved);
         }
         return discountGroups;
     }
 
-    private List<Integer> getOptimizedDiscountGroups(Map<Book, Integer> booksCountMap)
-    {
+    private List<Integer> getOptimizedDiscountGroups(Map<Book, Integer> booksCountMap) {
         discountGroups = getDiscountGroups(booksCountMap);
-        while (discountGroups.contains(3) && discountGroups.contains(5))
-        {
-            discountGroups.remove((Integer)3);
-            discountGroups.remove((Integer)5);
+        while (discountGroups.contains(3) && discountGroups.contains(5)) {
+            discountGroups.remove((Integer) 3);
+            discountGroups.remove((Integer) 5);
             discountGroups.add(4);
             discountGroups.add(4);
         }
@@ -70,16 +62,16 @@ public class BookBasket {
 
     /**
      * method who return the totalPrice with discount
+     *
      * @param books the list of books the customer wants to buy
      * @return totalPrice it's the price with discount
      */
-    public double getBookTotalPrice(List<Book> books){
+    public double getBookTotalPrice(List<Book> books) {
         double totalPrice = 0;
         booksCountMap = getBooksCountMap(books);
         discountGroups = getOptimizedDiscountGroups(booksCountMap);
-        for (Integer quantity : discountGroups)
-        {
-            double discountedGroupPrice = (BOOK_PRICE - this.discountRates.get(quantity)*BOOK_PRICE)*quantity;
+        for (Integer quantity : discountGroups) {
+            double discountedGroupPrice = (BOOK_PRICE - this.discountRates.get(quantity) * BOOK_PRICE) * quantity;
             totalPrice += discountedGroupPrice;
         }
         return totalPrice;
--- src/test/java/com/example/developmentbooks/BookBasketTest.java
+++ src/test/java/com/example/developmentbooks/BookBasketTest.java
@@ -13,12 +13,12 @@ class BookBasketTest {
     @Test
     void getBookTotalPriceFor1() {
         //GIVEN
-         BookBasket bookbasket = new BookBasket();
+        BookBasket bookbasket = new BookBasket();
         List<Book> books = new ArrayList<>();
         //WHEN
-        books.add(new Book("Title","Author",2022));
+        books.add(new Book("Title", "Author", 2022));
         //THEN
-        Assertions.assertEquals(50,bookbasket.getBookTotalPrice(books));
+        Assertions.assertEquals(50, bookbasket.getBookTotalPrice(books));
     }
 
     @Test
@@ -27,108 +27,110 @@ class BookBasketTest {
         BookBasket bookbasket = new BookBasket();
         List<Book> books = new ArrayList<>();
         //WHEN
-        books.add(new Book("Title","Author",2022));
-        books.add(new Book("Title2","Author",2022));
+        books.add(new Book("Title", "Author", 2022));
+        books.add(new Book("Title2", "Author", 2022));
         //THEN
-        Assertions.assertEquals(95,bookbasket.getBookTotalPrice(books));
+        Assertions.assertEquals(95, bookbasket.getBookTotalPrice(books));
     }
 
     @Test
     void getBookTotalPriceFor3Differents() {
         //GIVEN
-        BookBasket bookBasket= new BookBasket();
-        List<Book>books = new ArrayList<>();
+        BookBasket bookBasket = new BookBasket();
+        List<Book> books = new ArrayList<>();
 
         //WHEN
-        books.add(new Book("Title","Author",2022));
-        books.add(new Book("Title2","Author",2022));
-        books.add(new Book("Title","Author",2020));
+        books.add(new Book("Title", "Author", 2022));
+        books.add(new Book("Title2", "Author", 2022));
+        books.add(new Book("Title", "Author", 2020));
 
         //THEN
-        Assertions.assertEquals(135,bookBasket.getBookTotalPrice(books));
+        Assertions.assertEquals(135, bookBasket.getBookTotalPrice(books));
     }
 
     @Test
     void getBookTotalPriceFor4Differents() {
         //GIVEN
-        BookBasket bookBasket= new BookBasket();
-        List<Book>books = new ArrayList<>();
+        BookBasket bookBasket = new BookBasket();
+        List<Book> books = new ArrayList<>();
 
         //WHEN
-        books.add(new Book("Title","Author",2022));
-        books.add(new Book("Title2","Author",2022));
+        books.add(new Book("Title", "Author", 2022));
+        books.add(new Book("Title2", "Author", 2022));
         //Same title but other year
-        books.add(new Book("Title","Author",2020));
+        books.add(new Book("Title", "Author", 2020));
         //Same title but other author
-        books.add(new Book("Title","Author2",2020));
+        books.add(new Book("Title", "Author2", 2020));
 
         //THEN
-        Assertions.assertEquals(160,bookBasket.getBookTotalPrice(books));
+        Assertions.assertEquals(160, bookBasket.getBookTotalPrice(books));
     }
+
     @Test
     void getBookTotalPriceFor5Differents() {
         //GIVEN
-        BookBasket bookBasket= new BookBasket();
-        List<Book>books = new ArrayList<>();
+        BookBasket bookBasket = new BookBasket();
+        List<Book> books = new ArrayList<>();
 
         //WHEN
-        books.add(new Book("Title","Author",2022));
-        books.add(new Book("Title2","Author",2022));
+        books.add(new Book("Title", "Author", 2022));
+        books.add(new Book("Title2", "Author", 2022));
         //Same title but other year
-        books.add(new Book("Title","Author",2020));
+        books.add(new Book("Title", "Author", 2020));
         //Same title but other author
-        books.add(new Book("Title","Author2",2020));
+        books.add(new Book("Title", "Author2", 2020));
         //Same title but other year
-        books.add(new Book("Title3","Author2",2019));
+        books.add(new Book("Title3", "Author2", 2019));
 
         //THEN
-        Assertions.assertEquals(187.5,bookBasket.getBookTotalPrice(books));
+        Assertions.assertEquals(187.5, bookBasket.getBookTotalPrice(books));
     }
 
     @Test
     void getBookTotalPriceFor3DiffAnd1Duplicate() {
         //GIVEN
-        BookBasket bookBasket= new BookBasket();
-        List<Book>books = new ArrayList<>();
+        BookBasket bookBasket = new BookBasket();
+        List<Book> books = new ArrayList<>();
 
         //WHEN
-        books.add(new Book("Title","Author",2022));
+        books.add(new Book("Title", "Author", 2022));
         // It's a duplicate
-        books.add(new Book("Title","Author",2022));
+        books.add(new Book("Title", "Author", 2022));
 
-        books.add(new Book("Title2","Author",2022));
+        books.add(new Book("Title2", "Author", 2022));
         //Same title but other year
-        books.add(new Book("Title","Author",2020));
+        books.add(new Book("Title", "Author", 2020));
 
 
         //THEN
-        Assertions.assertEquals(185,bookBasket.getBookTotalPrice(books));
+        Assertions.assertEquals(185, bookBasket.getBookTotalPrice(books));
     }
+
     @Test
     void getBookTotalPriceFor5DiffAnd3Duplicate() {
         //GIVEN
-        BookBasket bookBasket= new BookBasket();
-        List<Book>books = new ArrayList<>();
+        BookBasket bookBasket = new BookBasket();
+        List<Book> books = new ArrayList<>();
 
         //WHEN
-        books.add(new Book("Title","Author",2022));
+        books.add(new Book("Title", "Author", 2022));
         // It's a duplicate
-        books.add(new Book("Title","Author",2022));
+        books.add(new Book("Title", "Author", 2022));
 
-        books.add(new Book("Title2","Author",2022));
+        books.add(new Book("Title2", "Author", 2022));
         // It's a duplicate
-        books.add(new Book("Title2","Author",2022));
+        books.add(new Book("Title2", "Author", 2022));
 
-        books.add(new Book("Title3","Author",2020));
+        books.add(new Book("Title3", "Author", 2020));
         // It's a duplicate
-        books.add(new Book("Title3","Author",2020));
+        books.add(new Book("Title3", "Author", 2020));
 
-        books.add(new Book("Title2","Author",2019));
+        books.add(new Book("Title2", "Author", 2019));
 
-        books.add(new Book("Title3","Author",2018));
+        books.add(new Book("Title3", "Author", 2018));
 
 
         //THEN
-        Assertions.assertEquals(320,bookBasket.getBookTotalPrice(books));
+        Assertions.assertEquals(320, bookBasket.getBookTotalPrice(books));
     }
 }
