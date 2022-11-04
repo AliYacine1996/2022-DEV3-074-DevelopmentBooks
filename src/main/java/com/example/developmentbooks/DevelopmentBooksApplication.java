d8f9b59fdbc6ebfc72a7d441a0c2e9d99a88af10
YacineALI <yacine.ali@sii.be>
2022-11-04 03:39:44 +0100

    add properties and run application

---
+++ src/main/java/com/example/developmentbooks/DevelopmentBooksApplication.java
@@ -0,0 +1,14 @@
+package com.example.developmentbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DevelopmentBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopmentBooksApplication.class, args);
+    }
+
+}
